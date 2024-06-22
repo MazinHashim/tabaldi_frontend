4079cf0cc5e517909afb1585e68967b36ef16677
maizn_hashim <maz.hash03@gmail.com>
2024-06-22 22:52:33 +0400

    passed environment endpoint url

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = 'https://dev.rateena-app.com/api/v1';
 export default axios.create({
-    baseURL: BASE_URL,
+    baseURL: process.env.REACT_APP_API_ENDPOINT,
     headers: { "Access-Control-Allow-Origin": '*' }
 });
 
