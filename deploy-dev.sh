b15c6a5e55173a1b8c3a9d17df98cedbda0cc003
maizn_hashim <maz.hash03@gmail.com>
2024-06-22 22:41:11 +0400

    changed build to qa

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,7 +2,7 @@ echo "Switching to master branch"
 git checkout master 
 
 echo "Building app..."
-npm run build
+npm run build:qa
 
 echo "Deploying files to server..."
 scp -r build/* root@194.238.22.144:/var/www/qa.rateena-app.com/
