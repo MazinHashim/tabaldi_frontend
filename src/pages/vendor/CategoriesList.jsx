6d3587cb42e84cabb800f4782e9b429719b280a1
maizn_hashim <maz.hash03@gmail.com>
2024-11-15 06:16:47 +0300

    fixed first category added issue

--- src/pages/vendor/CategoriesList.jsx
+++ src/pages/vendor/CategoriesList.jsx
@@ -80,12 +80,12 @@ const CategoriesList = ({routeRole}) => {
     }
 
     function onSetChangedData(category, wasEdit){
-        const otherCategories = categoryList.filter(cate=>cate.category.categoryId!==category.categoryId)
         if(wasEdit){
+            const otherCategories = categoryList.filter(cate=>cate.category.categoryId!==category.categoryId)
             const selected = categoryList.filter(cate=>cate.category.categoryId===category.categoryId)
             setChangeData({list: [...otherCategories, {...selected[0], category}]})
         } else {
-            setChangeData({list: [...otherCategories, {numberOfProducts:0, category}]})
+            setChangeData({list: [...{numberOfProducts:0, category}]})
         }
     }
     const queryCategories = categoryList?.filter((data) =>
